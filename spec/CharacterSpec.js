6c63af3d95949c7ee01f3137cfe784d881867538
duber <miguel@miguelcruz.net>
2016-06-18 13:32:30 +0200

    implemented iteration 1

--- spec/CharacterSpec.js
+++ spec/CharacterSpec.js
@@ -1,14 +1,75 @@
 describe("Character", function() {
 
-  it("Can deal damage", function(){
-      var offender = new Character();
-      var target = new Character();
-      var damage = 666;
+  it("Is born with 1000 health points", function(){
+    var character = new Character();
 
-      offender.attack(target, damage);
+    expect(character.health).toEqual(1000);
+  });
 
-      expect(target.health).toEqual(1000 - 666);
+  it("Dies if health is zero", function(){
+      var customInitialHealth = 0;
+      var character = new Character(customInitialHealth);
+
+      expect(character.alive()).toEqual(false);
+  });
+
+  describe("attacked", function(){
+    it("receives damage", function(){
+        var offender = new Character();
+        var target = new Character();
+        var damage = 666;
+
+        offender.attack(target, damage);
+
+        expect(target.health).toEqual(1000 - damage);
+    });
+
+    it("dies if receives more damage than its health", function(){
+        var offender = new Character();
+        var target = new Character();
+        var damage = 1001;
+
+        offender.attack(target, damage);
+
+        expect(target.alive()).toEqual(false);
+        expect(target.health).toEqual(0);
+    });
   })
 
+
+  describe("dead", function() {
+     it("cannot be healed", function(){
+      var customInitialHealth = 0;
+      var character = new Character(customInitialHealth);
+      var anyPoints = 66;
+
+      character.heal(anyPoints);
+      expect(character.alive()).toEqual(false);
+    });
+  });
+
+  describe("healed", function(){
+    it("recovers health", function(){
+        var customInitialHealth = 800;
+        var character = new Character(customInitialHealth);
+        var healPoints = 100;
+
+        character.heal(healPoints);
+
+        expect(character.health).toEqual(customInitialHealth + healPoints);
+    });
+
+    it("Cannot recover over 1000 points", function(){
+        var customInitialHealth = 1000;
+        var character = new Character(customInitialHealth);
+        var healPoints = 100;
+
+        character.heal(healPoints);
+
+        expect(character.health).toEqual(1000);
+    });
+  });
+
+ 
 
 });
--- src/Character.js
+++ src/Character.js
@@ -1,16 +1,44 @@
-function Character() {
+function Character(customInitialHealth) {
 	var INITIAL_HEALTH = 1000;
-	var health = INITIAL_HEALTH;
+	var MAX_HEALED_HEALTH = 1000;
+	var MIN_HEALTH = 0;
 
+	var initialHealth = function(customInitialHealth){
+		if (typeof customInitialHealth == 'undefined'){
+			return INITIAL_HEALTH 
+		}
+
+		return customInitialHealth;
+	};
+
+	var health = initialHealth(customInitialHealth);
+	
 	return {
    		attack: function(target, damage) {
    			target.receiveAttack(damage)
    		},
+		
 		health: health,
+		
 		receiveAttack: function(damage){
-			this.health = this.health - damage;
+			var resultantHealth = this.health - damage;
+			resultantHealth = (resultantHealth < MIN_HEALTH) ? MIN_HEALTH : resultantHealth;
+			this.health = resultantHealth;
+		},
+
+		heal: function(healthPoints){
+			if (!this.alive()){
+				return;
+			} 
+			var resultantHealth = this.health + healthPoints;
+			resultantHealth = (resultantHealth > MAX_HEALED_HEALTH) ? MAX_HEALED_HEALTH : resultantHealth;
+			this.health = resultantHealth;
+		},
+
+		alive: function(){
+			return (this.health > MIN_HEALTH);
 		}
 
 	}
-
 }
+
